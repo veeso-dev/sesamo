f2d1c1f4649d2c650c34fcf051a90d838d21ce64
veeso <christian.visintin@veeso.dev>
2023-10-01 19:04:46 +0200

    fix: send email

--- src/aws_ses.rs
+++ src/aws_ses.rs
@@ -23,27 +23,39 @@ impl AwsSesClient {
     ) -> anyhow::Result<()> {
         let client = aws_sdk_sesv2::Client::new(&self.aws_config);
         debug!("AWS SES client ready");
+
         let destination = Destination::builder()
-            .set_to_addresses(Some(recipients.iter().map(|s| s.to_string()).collect()))
+            .set_to_addresses(Some(vec![recipients
+                .iter()
+                .map(|s| s.to_string())
+                .collect()]))
             .build();
 
-        let body = Content::builder().data(content).charset("UTF-8").build();
-        let body = Body::builder().text(body).build();
-        let subject = Content::builder().data(subject).charset("UTF-8").build();
+        let subject_content = Content::builder().data(subject).charset("UTF-8").build();
+        let body_content = Content::builder().data(content).charset("UTF-8").build();
+        let body = Body::builder().html(body_content).build();
 
-        let message = Message::builder().subject(subject).body(body).build();
+        let msg = Message::builder()
+            .subject(subject_content)
+            .body(body)
+            .build();
 
-        let email_content = EmailContent::builder().simple(message).build();
+        let email_content = EmailContent::builder().simple(msg).build();
 
+        debug!("email content: {content}");
         info!("sending email from {sender} to {recipients:?}");
 
-        client
+        if let Err(err) = client
             .send_email()
             .from_email_address(sender)
             .destination(destination)
             .content(email_content)
             .send()
-            .await?;
+            .await
+        {
+            error!("Failed to send email: {err:?}");
+            anyhow::bail!(err);
+        }
 
         info!("email sent");
 
